982f21c760b47691a8e68b5c2679276386402d1b
Tatiana sanchez <tatianasanchezsoto@gmail.com>
2023-03-30 09:04:23 -0500

    updates clase 2

--- Clase2/index.js
+++ Clase2/index.js
@@ -9,17 +9,68 @@ const response = (res, message) => {
   res.end(`<p> ${message} </p>`);
 };
 
+function getProducts(res, json) {
+  res.setHeader("Content-Type", "application/json");
+  res.writeHead(200);
+  res.end(JSON.stringify(json));
+}
+
+const products = [
+  {
+    name: "Reloj",
+    price: 300,
+    quantity: 2,
+  },
+  {
+    name: "Correa",
+    price: 100,
+    quantity: 6,
+  },
+  {
+    name: "Sombrero",
+    price: 50,
+    quantity: 3,
+  },
+];
+
 const writeJSONResponse = (res, json) => {
   res.setHeader("Content-Type", "application/json");
   res.writeHead(200);
   res.end(JSON.stringify(json));
 };
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const url = req.url;
-  
+  let body = "";
+
+  await req.on("data", (chunk) => {
+    body += chunk;
+  });
+
   if (url === "/other") {
     response(res, "this is another route</p>");
+  } else if (url === "/api/v1/products") {
+    const method = req.method;
+    console.log("Method", method);
+    if (method === "GET") {
+      writeJSONResponse(res, products);
+    } else if (method === "POST") {
+      const product = JSON.parse(body);
+      products.push(product);
+      writeJSONResponse(res, products);
+    } else if (method === "DELETE") {
+      const productInformation = JSON.parse(body);
+      const productName = productInformation.name;
+      if (productName) {
+        const indexProduct = products.findIndex((p) => p.name == productName);
+
+        console.log("index of product", indexProduct);
+        if (indexProduct !== -1) {
+          products.splice(indexProduct, 1);
+        }
+        writeJSONResponse(res, products);
+      }
+    }
   } else {
     response(res, "HTML code");
   }

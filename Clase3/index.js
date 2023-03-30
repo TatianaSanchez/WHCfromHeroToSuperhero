b84b27d624e3b00c75e4d3fbc48f7db8058318b2
Tatiana sanchez <tatianasanchezsoto@gmail.com>
2023-03-30 09:13:10 -0500

    udpates clase 3 Rest API

--- Clase3/index.js
+++ Clase3/index.js
@@ -1,11 +1,22 @@
 const express = require("express");
-const { parse } = require("path");
-const port = 3000;
 
-const app = express();
 const productsData = require("./Data/products.json");
 const products = productsData.products;
 
+const port = 3000;
+const app = express();
+app.use(express.json());
+
+const errorLogger = (err, req, res, next) => {
+  console.log(err);
+  next(err);
+};
+
+const errorHandler = (err, req, res, next) => {
+  res.status(400).json({
+    message: err.message,
+  });
+};
 
 app.get("/", (request, response) => {
   response.send("Hello from Express!");
@@ -15,7 +26,7 @@ app.get("/api/v1/products", (request, response) => {
   response.json(products);
 });
 
-app.get("/api/v1/products/:productId", (request, response) => {
+app.get("/api/v1/products/:id", (request, response) => {
     const { productId } = request.params;
     const product = products.find(prod => prod.id === parseInt(productId));
     if(product){
@@ -25,6 +36,10 @@ app.get("/api/v1/products/:productId", (request, response) => {
         response.send(`Product whit ID ${productId} does not exist.`)
     }
 });
+
+app.use(errorLogger);
+app.use(errorHandler);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
